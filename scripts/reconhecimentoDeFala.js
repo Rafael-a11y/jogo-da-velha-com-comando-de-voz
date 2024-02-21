0e9c4a1789e936bb2ef8486b65fae47c3aff668c
Rafael Souto da Silva <rsowtto@gmail.com>
2024-02-20 21:36:01 -0300

    Adicionei alguns ouvintes para casos de erro e de não reconhecimento

--- scripts/reconhecimentoDeFala.js
+++ scripts/reconhecimentoDeFala.js
@@ -6,15 +6,16 @@ const SpeechRecognition =
   let recognitionAtivo = false;
   const recognition = new SpeechRecognition();
   const matrizPossibilidades = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
-  recognition.continuous = true;
   recognition.lang = "pt-BR";
+  // recognition.continuous = true;
   recognition.interimResults = false;
   recognition.maxAlternatives = 5;
   recognition.start();
-  recognitionAtivo = true;
-
+  
   recognition.addEventListener("result", onSpeak);
-
+  recognition.addEventListener("end", () => {recognition.start();})
+  recognition.addEventListener("nomatch", () => {console.log("Não entendi o que disse")})
+  recognition.addEventListener("error", (e) => {console.log("Houve um erro com o reconhecimento de fala: " + e.error)})
   function onSpeak(evento)
   {
     let chute = evento.results[evento.results.length - 1];
@@ -36,7 +37,7 @@ const SpeechRecognition =
     {
       for(let i = 0; i < chute.length; i++)
       {
-        let alternativa = chute[i].transcript.replaceAll(" ", "");
+        let alternativa = chute[i].transcript.replaceAll(" ", "").toUpperCase();
         if(valor == alternativa)
         {
           console.log(alternativa);
