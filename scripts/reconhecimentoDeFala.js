c372aa258956ebd1f67d06fdee6ec952c7d94a43
Rafael Souto da Silva <rsowtto@gmail.com>
2024-02-28 22:34:10 -0300

    Revert "Estilização e reconhecimento de voz melhorados"
    This reverts commit 0a4cd93caf39b067539a145933ba00d4de285439.

--- scripts/reconhecimentoDeFala.js
+++ scripts/reconhecimentoDeFala.js
@@ -5,26 +5,17 @@ const SpeechRecognition =
 
   let recognitionAtivo = false;
   const recognition = new SpeechRecognition();
-  const speechRecognitionList = new SpeechGrammarList();
-
-  const matrizPossibilidades = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"];
-  const grammar = '#JSGF V1.0; grammar casas; public <casa> = ' +
-  'A1 | A2 | A3 | B1 | B2 | B3 | C1 | C2 | C3 ;';
-
-  speechRecognitionList.addFromString(grammar, 1);
-  recognition.grammars = speechRecognitionList;
+  const matrizPossibilidades = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
   recognition.lang = "pt-BR";
-  recognition.continuous = false;
+  // recognition.continuous = true;
   recognition.interimResults = false;
   recognition.maxAlternatives = 5;
   recognition.start();
   
   recognition.addEventListener("result", onSpeak);
   recognition.addEventListener("end", () => {recognition.start();})
-  // recognition.addEventListener("nomatch", () => {console.log("Não entendi o que disse")})
-  // recognition.addEventListener("error", (e) => {console.log("Houve um erro com o reconhecimento de fala: " + e.error)})
-  recognition.onnomatch = () => {console.log("Não entendi o que disse")};
-  recognition.onerror = () => {console.log("Um erro ocorreu durante o reconhecimento de voz")};
+  recognition.addEventListener("nomatch", () => {console.log("Não entendi o que disse")})
+  recognition.addEventListener("error", (e) => {console.log("Houve um erro com o reconhecimento de fala: " + e.error)})
   function onSpeak(evento)
   {
     let chute = evento.results[evento.results.length - 1];
