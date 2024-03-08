a81197605dfe95fd44f922a760b019365d6ab195
Rafael Souto da Silva <rsowtto@gmail.com>
2024-03-07 23:46:58 -0300

    Funcionalidades aparentemente prontas, placar funcionando, tutorial funcionando, checkbox de 'Não mostrar tutorial novamente funcionando', falta melhorar o visual e aplicar teclas de atalho paar selecionar as casas

--- scripts/modais.js
+++ scripts/modais.js
@@ -1,19 +1,50 @@
 
+let placarO = document.querySelector("#pontuacao-o");
+let placarX = document.querySelector("#pontuacao-x");
+let exibeTutorial = sessionStorage.getItem("exibir-tutorial") || "true";
 const options = {keyboard: false, backdrop: "static", focus: true};
 const modalTutorial = new bootstrap.Modal('#tutorialBackdrop', options);
 const modalPlacar = new bootstrap.Modal("#placarModal", options);
 const modalPlacarDOM = document.getElementById("placarModal");
+const checkbox = document.querySelector("#checkbox-tutorial");
+console.log(checkbox);
+
 modalPlacarDOM.addEventListener('hidden.bs.modal', () => {reiniciar();});
+
+const naoMostrarTutorialNovamente = () =>
+{
+    if(checkbox.checked)
+    {
+        console.log("checkbox marcado");
+        sessionStorage.setItem("exibir-tutorial", false);
+    }
+    else
+    {
+        console.log("checkbox desmarcado")
+        sessionStorage.setItem("exibir-tutorial", true);
+    }
+}
+
+checkbox.addEventListener("change", naoMostrarTutorialNovamente);
 
 function exibirModalDeTutorial()
 {
-    modalTutorial.show();
+    if(exibeTutorial == "true")
+    {
+        modalTutorial.show();
+    }
+    else
+    {
+        console.log("tutrorial bloqueado")
+    }
+    
 }
 
 function exibirModalDePlacar()
 {
+    placarO.textContent = placar[0].pontuacao;
+    placarX.textContent = placar[1].pontuacao;
     modalPlacar.show();
 }
+
 exibirModalDeTutorial();
-
-
